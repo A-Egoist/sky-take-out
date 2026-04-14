9258ca8300b0375ea269800af104cdecc955d19c
A-Egoist <1012738513@qq.com>
2026-04-14 16:27:50 +0800

    feat: implement "delete dish"

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,5 +9,14 @@ import java.util.List;
 @Mapper
 public interface DishFlavorMapper {
 
+    /**
+     * 批量插入口味
+     */
     void insertBatch(List<DishFlavor> dishFlavors);
+
+    /**
+     * 根据菜品 id 删除对应的口味
+     */
+    @Delete("delete from dish_flavor where dish_id=#{dishId}")
+    void deleteByDishId(Long dishId);
 }
