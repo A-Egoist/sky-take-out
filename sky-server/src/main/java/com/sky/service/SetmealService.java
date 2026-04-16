425b13093b8c45432796b422bf5bf8798c0ecc03
A-Egoist <1012738513@qq.com>
2026-04-16 17:27:32 +0800

    feat: implement "setmeal page query"

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,10 +1,17 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
     /**
      * 新增套餐
      */
     void addSetmeal(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐分页查询
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
