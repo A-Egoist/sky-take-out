2a4e63e3baaea4f8c362f4ca7550df1b311c0aee
A-Egoist <1012738513@qq.com>
2026-06-05 22:16:29 +0800

    feat: implement order cancel function

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -21,6 +21,7 @@ import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -201,5 +202,47 @@ public class OrderServiceImpl implements OrderService {
         orderVO.setOrderDetailList(orderDetailList);
 
         return orderVO;
+    }
+
+    /**
+     * 用户取消订单
+     */
+    public void userCancelById(Long id) throws Exception {
+        // 根据 id 查询订单，ordersDB 只读
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+
+        // 订单状态：1 表示待付款、2 表示待接单、3 表示已接单、4 表示派送中、5 表示已完成、6 表示已取消
+        if (ordersDB.getStatus() > 2) {
+            // 状态 3、4、5 需要联系商家，让商家取消订单，用户无法直接取消这类订单
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Orders orders = new Orders();  // orders 只封装需要更新的字段，避免数据覆盖和数据混乱
+        orders.setId(ordersDB.getId());
+
+        // 订单处于待接单状态下取消，需要进行退款
+        if (ordersDB.getStatus().equals(Orders.TO_BE_CONFIRMED)) {
+            // 调用微信支付退款接口
+            // weChatPayUtil.refund(
+            //         ordersDB.getNumber(),  // 商户订单号
+            //         ordersDB.getNumber(),  // 商户退款订单号
+            //         new BigDecimal(0.01),  // 退款金额，单位 CNY
+            //         new BigDecimal(0.01)  // 退款金额
+            // );
+
+            // 支付状态修改为 退款
+            orders.setPayStatus(Orders.REFUND);
+        }
+
+        // 更新订单状态、取消原因、取消时间
+        orders.setStatus(Orders.CANCELLED);
+        orders.setCancelReason("用户取消");
+        orders.setCancelTime(LocalDateTime.now());
+        orderMapper.update(orders);
     }
 }
