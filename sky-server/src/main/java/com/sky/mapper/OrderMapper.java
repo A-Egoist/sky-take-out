e1a53feb5bd2f8b99e5350284a53b8076d70ebbb
A-Egoist <1012738513@qq.com>
2026-06-05 11:26:28 +0800

    feat: implement order payment function

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
@@ -10,4 +11,12 @@ public interface OrderMapper {
      * 插入订单数据，需要返回主键值
      */
     void insert(Orders orders);
+
+    /**
+     * 根据 orderNumber 和 userId 查询订单
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id = #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    void update(Orders orders);
 }
